a60bbbf0d3c62773669491d158179efe80637425
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-08-06 13:48:37 -0300

    feat: setup discord bot

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 
-	_ "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
 	log.Printf("Hello, world")
+
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
 }
