e6ae43da8ac854cad7981b2a731c14d18144b6f8
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-08-06 12:29:34 -0300

    chore: project setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "learning.rs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    templ.url = "github:a-h/templ?ref=v0.2.707";
+  };
+  outputs = { nixpkgs, ... } @ inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        f system pkgs);
+      templ = system: inputs.templ.packages.${system}.templ;
+    in
+    {
+      devShells = forAllSystems (system: pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            golangci-lint
+            (templ system)
+          ];
+        };
+      });
+    };
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/bwmarrin/discordgo"
+)
+
+func main() {
+}
