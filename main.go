4d248ee81fda18f3219d26e2990bc8a32807489e
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-08-07 14:36:55 -0300

    feat: basic message echoing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,9 +16,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.ChannelID == "1270407349869482006" {
+			_, err := s.ChannelMessageSend("1270407366617333920", m.Content)
+			log.Print(m.Content)
+			if err != nil {
+				panic(err)
+			}
+		}
+	})
+
 	err = discord.Open()
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not open session: %s", err)
 	}
+	log.Printf("Bot session opened successfully")
 
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT)
+	<-sig
+
+	err = discord.Close()
+	if err != nil {
+		log.Fatalf("could not close session: %s", err)
+	}
+	log.Printf("Bot session closed successfully")
 }
--- makefile
+++ makefile
@@ -6,15 +6,20 @@ lint:
 build:
 	go build -o bin/dislate
 
-dev:
+dev/watch:
 	go run github.com/joho/godotenv/cmd/godotenv@v1.5.1 \
 		go run github.com/air-verse/air@v1.52.2 \
 			--build.cmd "go build -o tmp/bin/dislate" \
 			--build.bin "tmp/bin/dislate" \
 			--build.include_ext "go" \
 			--build.stop_on_error "false" \
+			--build.send_interrupt "true" \
 			--misc.clean_on_exit true \
 			-- -p $(PORT) -d
+
+dev:
+	go run github.com/joho/godotenv/cmd/godotenv@v1.5.1 \
+		go run .
 
 run: build
 	./bin/dislate
