61c9f9aed80cc70c68380a364deeb9476ad98c15
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-08-26 20:40:06 -0300

    feat(events,messages): message edit/update support

--- internals/discord/bot/events.go
+++ internals/discord/bot/events.go
@@ -6,6 +6,7 @@ func (b *Bot) registerEventHandlers() {
 	ehs := []any{
 		events.NewGuildCreate(b.logger, b.db).Serve,
 		events.NewMessageCreate(b.db, b.translator).Serve,
+		events.NewMessageEdit(b.db, b.translator).Serve,
 		events.NewReady(b.logger, b.db).Serve,
 	}
 	for _, h := range ehs {
--- internals/discord/bot/events/messages.go
+++ internals/discord/bot/events/messages.go
@@ -23,10 +23,10 @@ func NewMessageCreate(db gconf.DB, t translator.Translator) MessageCreate {
 	return MessageCreate{db, t}
 }
 func (h MessageCreate) Serve(s *dgo.Session, ev *dgo.MessageCreate) {
-	log := gconf.GetLogger(ev.GuildID, s, h.db)
 	if ev.Message.Author.Bot {
 		return
 	}
+	log := gconf.GetLogger(ev.GuildID, s, h.db)
 
 	ch, err := h.db.Channel(ev.GuildID, ev.ChannelID)
 	if e.Is(err, guilddb.ErrNotFound) {
@@ -60,7 +60,7 @@ func (h MessageCreate) Serve(s *dgo.Session, ev *dgo.MessageCreate) {
 		return
 	}
 
-	_, err = h.getMessage(ev.Message, ch.Language)
+	_, err = getMessage(h.db, ev.Message, ch.Language)
 	if err != nil {
 		errors.NewErrDatabase(
 			slog.String("guild", ev.Message.GuildID),
@@ -76,7 +76,7 @@ func (h MessageCreate) Serve(s *dgo.Session, ev *dgo.MessageCreate) {
 			continue
 		}
 		go func(c guilddb.Channel) {
-			uw, err := h.getUserWebhook(s, c.ID, ev.Message.Author)
+			uw, err := getUserWebhook(s, c.ID, ev.Message.Author)
 			if err != nil {
 				errors.NewErrUserWebhook(
 					slog.String("guild", ev.Message.GuildID),
@@ -115,7 +115,7 @@ func (h MessageCreate) Serve(s *dgo.Session, ev *dgo.MessageCreate) {
 				tdm.GuildID = ev.Message.GuildID
 			}
 
-			_, err = h.getTranslatedMessage(tdm, ev.Message, c.Language)
+			_, err = getTranslatedMessage(h.db, tdm, ev.Message, c.Language)
 			if err != nil {
 				errors.NewErrDatabase(
 					slog.String("guild", ev.Message.GuildID),
@@ -130,7 +130,101 @@ func (h MessageCreate) Serve(s *dgo.Session, ev *dgo.MessageCreate) {
 
 }
 
-func (h MessageCreate) getUserWebhook(s *dgo.Session, channelID string, user *dgo.User) (*dgo.Webhook, error) {
+type MessageEdit struct {
+	db         gconf.DB
+	translator translator.Translator
+}
+
+func NewMessageEdit(db gconf.DB, t translator.Translator) MessageEdit {
+	return MessageEdit{db, t}
+}
+func (h MessageEdit) Serve(s *dgo.Session, ev *dgo.MessageUpdate) {
+	if ev.Message.Author.Bot {
+		return
+	}
+
+	log := gconf.GetLogger(ev.Message.GuildID, s, h.db)
+
+	msg, err := getMessage(h.db, ev.Message, lang.EN)
+	if e.Is(err, guilddb.ErrNotFound) {
+		log.Debug("Message is not in database, ignoring.",
+			slog.String("guild", ev.Message.GuildID),
+			slog.String("channel", ev.Message.ChannelID),
+		)
+		return
+	} else if err != nil {
+		errors.NewErrDatabase(
+			slog.String("guild", ev.Message.GuildID),
+			slog.String("channel", ev.Message.ChannelID),
+			slog.String("err", err.Error()),
+		).LogReply(log, s, ev.Message)
+		return
+	}
+
+	tmsgs, err := h.db.MessagesWithOrigin(msg.GuildID, msg.ChannelID, msg.ID)
+	if e.Is(err, guilddb.ErrNotFound) {
+		log.Debug("No translated message found, ignoring.",
+			slog.String("guild", ev.GuildID),
+			slog.String("channel", ev.ChannelID),
+		)
+		return
+	} else if err != nil {
+		errors.NewErrDatabase(
+			slog.String("guild", ev.GuildID),
+			slog.String("channel", ev.ChannelID),
+			slog.String("err", err.Error()),
+		).LogReply(log, s, ev.Message)
+		return
+	}
+
+	for _, m := range tmsgs {
+		if m.ID == msg.ID && m.GuildID == msg.GuildID {
+			continue
+		}
+		go func(m guilddb.Message) {
+			uw, err := getUserWebhook(s, m.ChannelID, ev.Message.Author)
+			if err != nil {
+				errors.NewErrUserWebhook(
+					slog.String("guild", ev.Message.GuildID),
+					slog.String("channel", ev.Message.ChannelID),
+					slog.Any("user", ev.Message.Author),
+					slog.String("err", err.Error()),
+				).LogReply(log, s, ev.Message)
+				return
+			}
+
+			t, err := h.translator.Translate(msg.Language, m.Language, ev.Message.Content)
+			if err != nil {
+				errors.New("Error while trying to translate message",
+					slog.String("guild", ev.Message.GuildID),
+					slog.String("channel", ev.Message.ChannelID),
+					slog.String("message", ev.Message.ID),
+					slog.String("content", ev.Message.Content),
+					slog.String("err", err.Error()),
+				).LogReply(log, s, ev.Message)
+				return
+			}
+
+			_, err = s.WebhookMessageEdit(uw.ID, uw.Token, m.ID, &dgo.WebhookEdit{
+				Content: &t,
+			})
+			if err != nil {
+				errors.NewErrUserWebhook(
+					slog.String("guild", ev.Message.GuildID),
+					slog.String("channel", ev.Message.ChannelID),
+					slog.String("message", ev.Message.ID),
+					slog.String("content", ev.Message.Content),
+					slog.String("err", err.Error()),
+				).LogReply(log, s, ev.Message)
+				return
+			}
+		}(m)
+
+	}
+
+}
+
+func getUserWebhook(s *dgo.Session, channelID string, user *dgo.User) (*dgo.Webhook, error) {
 	var whName = "DISLATE_USER_WEBHOOK_" + user.ID
 
 	ws, err := s.ChannelWebhooks(channelID)
@@ -153,14 +247,14 @@ func (h MessageCreate) getUserWebhook(s *dgo.Session, channelID string, user *dg
 	return w, nil
 }
 
-func (h MessageCreate) getMessage(m *dgo.Message, lang lang.Language) (guilddb.Message, error) {
-	msg, err := h.db.Message(m.GuildID, m.ChannelID, m.ID)
+func  getMessage(db gconf.DB, m *dgo.Message, lang lang.Language) (guilddb.Message, error) {
+	msg, err := db.Message(m.GuildID, m.ChannelID, m.ID)
 
 	if e.Is(err, guilddb.ErrNotFound) {
-		if err := h.db.MessageInsert(guilddb.NewMessage(m.GuildID, m.ChannelID, m.ID, lang)); err != nil {
+		if err := db.MessageInsert(guilddb.NewMessage(m.GuildID, m.ChannelID, m.ID, lang)); err != nil {
 			return guilddb.Message{}, err
 		}
-		msg, err = h.db.Message(m.GuildID, m.ChannelID, m.ID)
+		msg, err = db.Message(m.GuildID, m.ChannelID, m.ID)
 		if err != nil {
 			return guilddb.Message{}, err
 		}
@@ -170,11 +264,11 @@ func (h MessageCreate) getMessage(m *dgo.Message, lang lang.Language) (guilddb.M
 
 }
 
-func (h MessageCreate) getTranslatedMessage(m, original *dgo.Message, lang lang.Language) (guilddb.Message, error) {
-	msg, err := h.db.Message(m.GuildID, m.ChannelID, m.ID)
+func  getTranslatedMessage(db gconf.DB, m, original *dgo.Message, lang lang.Language) (guilddb.Message, error) {
+	msg, err := db.Message(m.GuildID, m.ChannelID, m.ID)
 
 	if e.Is(err, guilddb.ErrNotFound) {
-		if err := h.db.MessageInsert(guilddb.NewTranslatedMessage(
+		if err :=db.MessageInsert(guilddb.NewTranslatedMessage(
 			m.GuildID,
 			m.ChannelID,
 			m.ID,
@@ -184,7 +278,7 @@ func (h MessageCreate) getTranslatedMessage(m, original *dgo.Message, lang lang.
 		)); err != nil {
 			return guilddb.Message{}, err
 		}
-		msg, err = h.db.Message(m.GuildID, m.ChannelID, m.ID)
+		msg, err =db.Message(m.GuildID, m.ChannelID, m.ID)
 		if err != nil {
 			return guilddb.Message{}, err
 		}
