f70de4bca19e6157bff064c0b0708da11fa5f666
Gustavo "Guz" L. de Mello <contact@guz.one>
2024-09-02 17:47:23 -0300

    feat: rename evErr to everr

--- internals/discord/bot/events/guild.go
+++ internals/discord/bot/events/guild.go
@@ -23,12 +23,12 @@ func NewGuildCreate(log *slog.Logger, db gconf.DB) GuildCreate {
 func (h GuildCreate) Serve(s *dgo.Session, ev *dgo.GuildCreate) {
 	err := h.db.GuildInsert(gdb.Guild[gconf.ConfigString]{ID: ev.Guild.ID})
 
-	evErr := errors.NewEventError[GuildCreate](map[string]any{
+	everr := errors.NewEventError[GuildCreate](map[string]any{
 		"GuildID": ev.Guild.ID,
 	})
 
 	if err != nil && !e.Is(err, gdb.ErrNoAffect) {
-		evErr.Wrapf("Failed to add guild to database", err).Log(h.log)
+		everr.Wrapf("Failed to add guild to database", err).Log(h.log)
 	} else if err != nil {
 		h.log.Info("Guild already in database", slog.String("id", ev.Guild.ID))
 	} else {
@@ -46,13 +46,13 @@ func NewReady(log *slog.Logger, db gconf.DB) EventHandler[*dgo.Ready] {
 }
 
 func (h Ready) Serve(s *dgo.Session, ev *dgo.Ready) {
-	evErr := errors.NewEventError[GuildCreate](map[string]any{})
+	everr := errors.NewEventError[GuildCreate](map[string]any{})
 
 	for _, g := range ev.Guilds {
 		err := h.db.GuildInsert(gdb.Guild[gconf.ConfigString]{ID: g.ID})
 
 		if err != nil && !e.Is(err, gdb.ErrNoAffect) {
-			evErr.Wrapf("Failed to add guild to database", err).AddData("GuildID", g.ID).Log(h.log)
+			everr.Wrapf("Failed to add guild to database", err).AddData("GuildID", g.ID).Log(h.log)
 		} else if err != nil {
 			h.log.Info("Guild already in database", slog.String("id", g.ID))
 		} else {
